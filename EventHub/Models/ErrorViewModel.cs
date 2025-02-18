7935547ff4a671bc945b61ee7f7cb69e0cab3811
JOSEE <nhan321hd7e@gmail.com>
2025-02-18 21:07:53 +0700

    Modified project structure

--- EventHub/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EventHub.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

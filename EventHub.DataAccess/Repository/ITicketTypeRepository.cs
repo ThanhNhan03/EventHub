056bb0b7e8f71ffd74d06445b5e9905d033282c8
pnam21 <phamnamcwd@gmail.com>
2025-02-18 21:16:55 +0700

    add repos for event ticket

---
+++ EventHub.DataAccess/Repository/ITicketTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using EventHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHub.DataAccess.Repository
+{
+    public interface ITicketTypeRepository
+    {
+        Task<int> UpdateTicketTypeAsync(TicketType ticketType);
+        Task<IEnumerable<Ticket>> GetTicketsByTickeyTypeIdAsync(int ticketTypeId);
+        Task<TicketType?> GetTicketTypeByIdAsync(int ticketTypeId);
+    }
+}

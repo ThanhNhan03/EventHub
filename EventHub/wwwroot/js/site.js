24aba15ac36c28a7ad8d62c8d2f81e53150c4c85
JOSEE <nhan321hd7e@gmail.com>
2025-02-19 21:22:19 +0700

    Fixed layout view

--- EventHub/wwwroot/js/site.js
+++ EventHub/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    console.log("running....");
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    $('.counter-count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text().replace(/,/g, '') // Remove any existing commas before animating
+        }, {
+            duration: (Math.random() + 0.5) * 3000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(numberWithCommas(Math.ceil(now)));
+            }
+        });
+    });
+});
